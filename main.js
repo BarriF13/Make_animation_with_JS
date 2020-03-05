ec686056aa19e0050acc4e7219a5607fac78ad89
Student <student@Makerss-MacBook-Air-5.local>
2020-03-05 17:24:49 +0000

    Redirect page set up

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 //setInterval
 
 var counter = 10;
-
-
-
-
+var item = document.getElementById("nav");
+  item.innerHTML = "you will redirect to another page in "+counter+" second";
 
 function show()
 {
-    counter++;
-    alert('Animation yay');
-    if (counter == 5){
+    counter--;
+    item.innerHTML = "you will redirect to another page in "+counter+" second";
+
+    if(counter ==0){
       clearInterval(timer);
+      window.location= "https://www.cats.org.uk/adopt-a-cat/choosing-a-cat";
     }
+
 }
 //setTimeout(show, 2000); works only once
 //setInterval(show, 2000); keeps coming
-var timer = setInterval(show, 2000);
+var timer = setInterval(show, 1000);
