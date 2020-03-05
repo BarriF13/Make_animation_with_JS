13358a61d1b0784ffea4acc1b133a14df9b99eff
Student <student@Makerss-MacBook-Air-5.local>
2020-03-05 17:15:59 +0000

    set up counter

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('Animation yay');
+//setInterval
+
+var counter = 10;
+
+
+
+
+
+function show()
+{
+    counter++;
+    alert('Animation yay');
+    if (counter == 5){
+      clearInterval(timer);
+    }
+}
+//setTimeout(show, 2000); works only once
+//setInterval(show, 2000); keeps coming
+var timer = setInterval(show, 2000);
